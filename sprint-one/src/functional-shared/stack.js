005234059c1438b15be081bc7e35b73486b8ecc7
Rebecca Yip <rebz135@gmail.com>
2018-08-01 13:25:27 -0700

    completed up to stack functional shared

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,29 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var stack = {};
+  stack.count = 0;
+  stack.storage = {};
+  _.extend(stack, stackMethods);
+  return stack;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  size: function (){
+    if (this.count <0) {
+      return 0;
+    } else {
+      return this.count;
+    }
+  },
+  push: function (item){
+    this.storage[this.count] = item;
+    this.count++;
+  },
 
+  pop: function() {
+    this.count --;
+    return this.storage[this.count];
+  }
 
+};
