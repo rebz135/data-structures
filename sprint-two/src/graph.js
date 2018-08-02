fe9226eb47f71bbdad75621960492d62f893ff0c
rebz135 <rebz135@gmail.com>
2018-08-02 11:28:10 -0700

    compelted remove nodes on graph

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -2,26 +2,42 @@
 
 // Instantiate a new graph
 var Graph = function() {
+  this.nodes = {};
 };
 
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
+  let obj = {};
+  obj.edges = [];
+  this.nodes[node] = obj;
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
+  if (this.nodes[node]) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
+  if (this.nodes[node]) {
+    delete this.nodes[node];
+  }
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
+  // console.log(this.nodes[fromNode].edges + ' || ' + this.nodes[toNode].edges);
+  return this.nodes[fromNode].edges.indexOf(toNode) > 0;
 };
 
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
+  this.nodes[fromNode].edges.push(toNode);
+  this.nodes[toNode].edges.push(fromNode);
 };
 
 // Remove an edge between any two specified (by value) nodes.
@@ -35,5 +51,3 @@ Graph.prototype.forEachNode = function(cb) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
-
-
