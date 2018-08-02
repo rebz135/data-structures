eed60dac0dd4862940fd4b365e03e47b35c8d335
rebz135 <rebz135@gmail.com>
2018-08-02 16:15:02 -0700

    completed graph

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -24,6 +24,10 @@ Graph.prototype.contains = function(node) {
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
   if (this.nodes[node]) {
+    let nodeEdges = this.nodes[node].edges;
+    for (let i = 0; i < nodeEdges.length; i++) {
+      this.removeEdge(node, nodeEdges[i]);
+    }
     delete this.nodes[node];
   }
 };
@@ -31,7 +35,7 @@ Graph.prototype.removeNode = function(node) {
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
   // console.log(this.nodes[fromNode].edges + ' || ' + this.nodes[toNode].edges);
-  return this.nodes[fromNode].edges.indexOf(toNode) > 0;
+  return this.nodes[fromNode].edges.indexOf(toNode) >= 0;
 };
 
 // Connects two nodes in a graph by adding an edge between them.
@@ -42,12 +46,28 @@ Graph.prototype.addEdge = function(fromNode, toNode) {
 
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
+  let delFromIndex = this.nodes[fromNode].edges.indexOf(toNode);
+  let delToIndex = this.nodes[toNode].edges.indexOf(fromNode);
+  this.nodes[fromNode].edges.splice(delFromIndex,1);
+  this.nodes[toNode].edges.splice(delToIndex,1);
 };
 
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
+  _.each(this.nodes, function(val, key) {
+    cb(key);
+  });
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ Graph: O(1)
+ addNode: O(1)
+ removeNode: O(n^2)
+ hasEdge: O(n)
+ addEdge: O(n)
+ removeEdge: O(n)
+ forEachNode: O(n)
+ 
+ Note: if want to reduce time complexity, refactor with edges as object instead of array
  */
