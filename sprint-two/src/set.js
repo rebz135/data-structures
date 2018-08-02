306246dd3924ba6a69aa02e1c5d448c10e718a90
rebz135 <rebz135@gmail.com>
2018-08-02 16:25:15 -0700

    Pass set tests in spec runner

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,31 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {}; // fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  this._storage[item] = 1;
 };
 
 setPrototype.contains = function(item) {
+  if (this._storage[item]) {
+    return true;
+  }
+  return false;
 };
 
 setPrototype.remove = function(item) {
+  delete this._storage[item];
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ 
+ Set.add: O(1)
+ Set.contains: O(1)
+ Set.remove: O(1)
+
  */
