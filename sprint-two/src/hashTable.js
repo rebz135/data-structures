d24f0c61f8335f797d6b751379657f3a1ef2ee13
rebz135 <rebz135@gmail.com>
2018-08-02 18:27:35 -0700

    completed hash table up till collisions

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,14 +7,22 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  let val = this._storage.retrieve(index);
+  if (val) { 
+    extend(val, {k: v});
+    this._storage.set(index, val);
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  return this._storage.get(index[k]);
 };
+
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.set(index, undefined);
 };
 
 
