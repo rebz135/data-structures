6373cfc4bbf8b4f8f15f3fe151b7ebb6fe5ca3fc
rebz135 <rebz135@gmail.com>
2018-08-03 11:28:27 -0700

    working on resizing for hash table

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -3,34 +3,63 @@
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  this.totalElements = 0;
 };
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  let keyVal = {};
-  
-  keyVal[k] = v;
-  let val = this._storage.get(index);
-  if (val) { 
-    _.extend(val, keyVal);
-    this._storage.set(index, val);
-  } else {
-    this._storage.set(index, keyVal);
-  }
+  this.totalElements++;
+  //double in size when totalElements = 75% of limit
+  this.resize();
+  let bin = this._storage.get(index) || [];
+  bin.push([k,v])
+  this._storage.set(index, bin);
+  this.totalElements++;
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  return this._storage.get(index)[k];
+  let bin = this._storage.get(index) || [];
+  let result = undefined;
+  bin.forEach(function(ele){
+    if (ele[0] === k) {
+      result = ele[1];
+    }
+    });
+  return result;
 };
-
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  let obj = this._storage.get(index);
-  delete obj[k];
-  this._storage.set(index, obj);
+  this.totalElements--;
+  //half when totalElements = 25% of limit
+
+  let bin = this._storage.get(index);
+  let delIndex = -1;
+  bin.forEach(function(ele, index){
+    if (ele[0] === k) {
+      delIndex = index;
+    }
+  })
+  bin.splice(delIndex,1);
 };
+
+HashTable.prototype.resize =  function() {
+  if (this.totalElements > 0.75 * this._limit) {
+    let newStorage = LimitedArray(this._limit * 2);
+    for (var i = 0; i<this._limit; i++) {
+      let bin = this._storage.get(i) || [];
+      bin.forEach(function(ele, index){
+        var index = getIndexBelowMaxForKey(ele[0], this._limit * 2)
+        newStorage.set(index, [ele[0], ele[1]])
+      });
+    }
+    console.log(this._limit);
+    this._storage = newStorage;
+    this._limit = this._limit * 2;
+    console.log(this._limit);
+  }
+}
 
 /*
  * Complexity: What is the time complexity of the above functions?
